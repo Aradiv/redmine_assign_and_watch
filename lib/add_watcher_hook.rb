0263d8502b168382311f225c077aa42391420e78
Eric Brueggemann <e.brueggemann@frankfurt-main.ihk.de>
2013-04-24 15:30:33 +0200

    forward entire context

--- lib/add_watcher_hook.rb
+++ lib/add_watcher_hook.rb
@@ -1,13 +1,14 @@
 class AddWatcherHook < Redmine::Hook::ViewListener
 	def controller_issues_edit_before_save(context = { })
-		addWatcher(context[:issue])
+		addWatcher(context)
 	end
 	
 	def controller_issues_new_before_save(context = { })
-		addWatcher(context[:issue])
+		addWatcher(context)
 	end
 	
-	def addWatcher(@issue)
+	def addWatcher(context = { })
+		@issue = context[:issue]
 		unless @issue.assigned_to == nil || @issue.watched_by?(@issue.assigned_to) || @issue.author == @issue.assigned_to
 			@issue.add_watcher(@issue.assigned_to)
 		end
